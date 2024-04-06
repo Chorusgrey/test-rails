f59b8e660b6c173d039a4e4ca1fcf8c641603428
Perrine Dohr <antartikat@gmail.com>
2024-04-06 15:19:37 +0200

    routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+  has_many :cars, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  validates :first_name, :last_name, :email, presence: true
 end
